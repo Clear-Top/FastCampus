3e600897527f378ef9a1ae3fb4ec04e5e2752b35
Kim <6tank1004@gmail.com>
2023-08-08 01:32:41 +0900

    해결완료

--- 문제풀이/(17298-골드4) 오큰수 (성공).py
+++ 문제풀이/(17298-골드4) 오큰수 (성공).py
@@ -1,6 +1,5 @@
 import sys
 input = sys.stdin.readline
-# sys.setrecursionlimit(10**6)
 
 n = int(input().strip())
 
@@ -10,18 +9,8 @@ stack = []
 
 for idx, num in enumerate(data):
     # print(num, idx)
-    if not stack:
-        stack.append((num, idx))    # [0]: 수, [1]: 인덱스
-        continue
-    # if stack[-1][0] > num: # 작은 수가 들어오면 push (틀림)
-    if stack[-1][0] >= num: # 작거나 같은 수가 들어오면 push (수정)
-        stack.append((num, idx))
-    elif stack[-1][0] < num:
-        while stack:
-            if stack[-1][0] < num:
-                process = stack.pop()
-                오큰수[process[1]] = num
-                continue
-            break
-        stack.append((num, idx))
+    while stack and stack[-1][0] < num:
+        previous = stack.pop()
+        오큰수[previous[1]] = num
+    stack.append((num, idx))
 print(*오큰수)
