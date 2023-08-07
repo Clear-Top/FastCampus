4324d5df6549457ec95ced430c96e67a5d0f1f7c
Kim <6tank1004@gmail.com>
2023-08-07 16:33:34 +0900

    Accept (시간개선필요)

--- 문제풀이/(17298-골드4) 오큰수 (성공).py
+++ 문제풀이/(17298-골드4) 오큰수 (성공).py
@@ -1,20 +1,27 @@
 import sys
-from collections import deque
-from itertools import islice
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+# sys.setrecursionlimit(10**6)
 
 n = int(input().strip())
 
-data = deque(map(int, input().strip().split(' ')))
-# print(n, data)
+data = list(map(int, input().strip().split(' ')))
+오큰수 = [-1] * n
+stack = []
 
-for i in range(n):
-    pop_data = data.popleft()
-    result_num = -1
-    for num in islice(data, 0, len(data), 1):
-        # print(f'{pop_data} vs {num}')
-        if pop_data < num:
-            result_num = num
+for idx, num in enumerate(data):
+    # print(num, idx)
+    if not stack:
+        stack.append((num, idx))    # [0]: 수, [1]: 인덱스
+        continue
+    # if stack[-1][0] > num: # 작은 수가 들어오면 push (틀림)
+    if stack[-1][0] >= num: # 작거나 같은 수가 들어오면 push (수정)
+        stack.append((num, idx))
+    elif stack[-1][0] < num:
+        while stack:
+            if stack[-1][0] < num:
+                process = stack.pop()
+                오큰수[process[1]] = num
+                continue
             break
-    print(result_num, end=' ')
+        stack.append((num, idx))
+print(*오큰수)
