ba7aaa48deb3480b8579230b9e8296436266f059
김현모 <6tank1004@gmail.com>
2023-09-08 19:16:02 +0900

    완전탐색 에라토스테네스의 체 사용 (약 O(n))

--- SK 텔레콤준비/1. 완전탐색 (Brute Force)/Lv2_소수찾기.py
+++ SK 텔레콤준비/1. 완전탐색 (Brute Force)/Lv2_소수찾기.py
@@ -1,48 +1,44 @@
 # 문제: https://school.programmers.co.kr/learn/courses/30/lessons/42839
 
-# 해결여부: 
-# 해결시간: 
+# 해결여부: O (시간초과 => '에라토스테네스의 체'로 해결)
+# 해결시간: 30분초과
 
 '''
-한 자릿수 종이조각 이어붙여서 소수를 몇개 만들수 있는지 => eval()
-
-종이조각은 7개니까 combination 가능할 듯?
+종이조각은 7개니까 itertools의 permutations 가능할 듯?
+순열은 O(n!) 이기 때문에 n<=10 이면 가능함
 '''
 
 from itertools import permutations
 
-def find_prim(str_lists):
-    result = 0
-    sets = set()
-    for str_list in str_lists:
-        number = int(''.join(str_list))
-        if number == 0 or number == 1:
-            continue
-        print(f"{number} 추가")
-        sets.add(number)
-    print(sets)
-    for x in sets:
-        flag = True
-        for i in range(2, x//2+1):
-            if x % i == 0:
-                flag = False
-        if flag:
-            result += 1
-        print(f"소수: {x} => count: {result}")
-    return result
+def eratos(number): 
+    ''' (에라토스테네스의 체)
+    # O(nlog(longn)) = O(n) 만큼의 성능
+    # 소수가 아닌것들의 배수들은 검토할 필요가 없다. 
+        => 수 A가 주어졌을 떄, sqrt(A) 까지만 돌면서 나눠지는지 확인한다.
+    '''
+    for i in range(2, int(number**(0.5)) + 1):
+        if number % i == 0:
+            return 0
+    return 1
 
 def solution(numbers):
-    # numbers : string
-
-    count = 0
-    lists = []
+    # count = 0
+    answer = 0
+    lists = list()
     for i in range(1, len(numbers) + 1):
-        lists += list(permutations(numbers, i))
-    count += find_prim(lists)
-    return count
+        lists += list(map(int, map("".join, permutations(numbers, i))))
+    # count += find_prim(set(lists))
+
+    p = set(lists)
+    for lst in p:
+        if lst < 2:
+            continue
+        val = eratos(lst)
+        if val:
+            print(f"{lst}는 소수입니다.")
+            answer += val
+    return answer
 
 
 # print(solution('17'))
 print(solution("011"))
-
-# 조합한걸 하나의 set에 넣어서 중복을 없애야 함
